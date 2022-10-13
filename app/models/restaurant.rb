e9e85a45362fbbb5903226b782c49a546578aa34
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-13 21:06:07 +0200

    generate and fill the restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# documentation comment
 class Restaurant < ApplicationRecord
+  # rubocop:disable Style/MutableConstant
+  CATEGORIES = %w[chinese italian japanese french belgian]
+  # rubocop:enable Style/MutableConstant
+
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
