37f2a7a08d6151524d810249203bf51a7814aae8
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-13 21:09:15 +0200

    create seeds for the restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,41 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+
+Restaurant.create!(
+  name: "C'est Bon C'est Belge",
+  address: 'Rue du Bon-Secours, Brussel',
+  phone_number: '+321 23 45 67 89',
+  category: 'belgian'
+)
+
+Restaurant.create!(
+  name: 'Epicure',
+  address: 'Faubourg Saint Honoré, Paris',
+  phone_number: '+331 23 45 67 89',
+  category: 'french'
+)
+
+Restaurant.create!(
+  name: 'Ristorante A Mano',
+  address: 'Strausberger Platz, Berlin',
+  phone_number: '+491 23 45 67 89',
+  category: 'italian'
+)
+
+Restaurant.create!(
+  name: 'Teppanyaki Restaurant Sazanka',
+  address: 'Ferdinand Bolstraat, Amsterdam',
+  phone_number: '+311 23 45 67 89',
+  category: 'japanese'
+)
+
+Restaurant.create!(
+  name: 'Sichuan Folk',
+  address: 'Shoreditch, London',
+  phone_number: '+441 23 45 67 89',
+  category: 'chinese'
+)
