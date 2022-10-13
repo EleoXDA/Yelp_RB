6716b61a75403b820261975321ba87c8671ecd0f
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-13 21:06:52 +0200

    generate and fill the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }
 end
